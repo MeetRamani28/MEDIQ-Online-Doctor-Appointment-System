d97a7ee3be6333356f33ace1b2da3c6199f45461
Meet Ramani <meetramani21@gmail.com>
2026-06-19 18:00:27 +0530

    implement loading state to prevent white screen

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,16 +3,21 @@ import { useDispatch } from "react-redux";
 import { fetchUserProfile } from "./features/auth/authThunks";
 import { useSelector } from "react-redux";
 import Routing from "./routes/Routing";
+import LoadingScreen from "./components/atoms/LoadingScreen";
 
 function App() {
   const dispatch = useDispatch();
-  const { isAuthenticated } = useSelector((state) => state.auth);
+  const { loading, isAuthenticated } = useSelector((state) => state.auth);
 
   useEffect(() => {
     if (!isAuthenticated) {
       dispatch(fetchUserProfile());
     }
   }, [dispatch, isAuthenticated]);
+
+  if (loading) {
+    return <LoadingScreen />;
+  }
 
   return (
     <div className="font-[gilroy]">
